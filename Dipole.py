93ac9ca38553a4604c591c13e75514e697d86799
shainyoreng <amit.lanis3@gmail.com>
2024-09-22 11:54:47 +0300

    commented code

--- Dipole.py
+++ Dipole.py
@@ -1,50 +1,75 @@
 from Vector2 import Vector2
 from math import sin, cos
 
-
 class Dipole2D:
+	def __init__(self, loc, theta, R, size, moi, center_vector):
+		"""
+		Initialize the Dipole2D object.
+		
+		:param loc: Initial location of the dipole
+		:param theta: Initial orientation angle of the dipole
+		:param R: Some parameter R (not used in the current implementation)
+		:param size: Size of the dipole
+		:param moi: Moment of inertia of the dipole
+		:param center_vector: Vector representing the center of the dipole
+		"""
+		self.size = size
+		self.moi = moi
+		self.loc = loc
+		self.theta = theta
+		self.R = R
+		self.omega: float = 0  # Angular velocity of the dipole
+		self.find_moment_in_cartesian()  # Calculate the moment in Cartesian coordinates
+		self.center_vector = center_vector
 
-    def __init__(self, loc, theta, R, size, moi,center_vector):
-        self.size = size
-        self.moi = moi
-        self.loc = loc
-        self.theta = theta
-        self.R = R
-        self.omega: float = 0
-        self.find_moment_in_cartesian()
-        self.center_vector = center_vector
+	def find_moment_in_cartesian(self):
+		"""
+		Calculate the moment of the dipole in Cartesian coordinates.
+		"""
+		self.moment = Vector2(sin(self.theta), cos(self.theta)) * self.size
 
+	def center_loc(self):
+		"""
+		Returns the actual location of the moment of the dipole.
+		
+		:return: Vector representing the center location of the dipole
+		"""
+		return self.loc + self.center_vector.rotate(-self.theta)
 
-    def find_moment_in_cartesian(self):
-        self.moment = Vector2(sin(self.theta), cos(self.theta)) * self.size
+	def get_field(self, loc: Vector2):
+		"""
+		Returns the field the dipole induces at a specific point.
+		
+		:param loc: Location at which to calculate the field
+		:return: Vector representing the induced field at the given location
+		"""
+		if loc == self.center_loc():
+			return Vector2()  # Return zero vector if the location is at the center
 
+		r_hat: Vector2 = loc - self.center_loc()  # Vector from dipole center to the location
+		r = abs(r_hat)  # Distance from dipole center to the location
+		r_hat /= r  # Normalize r_hat to get the unit vector
 
-    def center_loc(self):
-        """returns the actual location of the moment of the dipole"""
-        return self.loc + self.center_vector.rotate(-self.theta)
+		# Calculate the field using the dipole field formula
+		return (r_hat * 3 * (self.moment * r_hat) - self.moment) / r**3
 
-
-    def get_field(self, loc: Vector2):
-        """returns the field the dipole induces at a specific point"""
-
-        if loc == self.center_loc():
-            return Vector2()
-
-        r_hat: Vector2 = loc - self.center_loc()
-        r = abs(r_hat)
-        r_hat /= r
-
-        return (r_hat * 3 * (self.moment * r_hat) - self.moment) / r**3
-
-    def update(self, dt, B_field: Vector2, external_moment: float = 0, resistance=0):
-        """updates the orientation ond angular velocity of the dipole"""
-        force_moment: float = self.moment.cross(B_field) + external_moment - resistance * self.omega
-        self.theta += self.omega * dt
-        self.find_moment_in_cartesian()
-        self.omega += dt * force_moment / self.moi
-
-
-
-
-
-
+	def update(self, dt, B_field: Vector2, external_moment: float = 0, resistance=0):
+		"""
+		Updates the orientation and angular velocity of the dipole.
+		
+		:param dt: Time step for the update
+		:param B_field: External magnetic field vector
+		:param external_moment: External moment applied to the dipole
+		:param resistance: Resistance to the dipole's motion
+		"""
+		# Calculate the net moment acting on the dipole
+		force_moment: float = self.moment.cross(B_field) + external_moment - resistance * self.omega
+		
+		# Update the orientation angle based on the current angular velocity
+		self.theta += self.omega * dt
+		
+		# Recalculate the moment in Cartesian coordinates
+		self.find_moment_in_cartesian()
+		
+		# Update the angular velocity based on the net moment and moment of inertia
+		self.omega += dt * force_moment / self.moi
--- Vector2.py
+++ Vector2.py
@@ -2,6 +2,12 @@ from math import sin ,cos
 
 
 class Vector2:
+    """
+    A class to represent a 2-dimensional vector.
+    Attributes:
+        x (float): The x-coordinate of the vector.
+        y (float): The y-coordinate of the vector.
+    """
 
     def __init__(self, x: float = 0, y: float = 0):
         self.x = x
@@ -23,18 +29,23 @@ class Vector2:
             return Vector2(self.x * other, self.y * other)
 
     def __truediv__(self, other):
+        """Divides the vector by a scalar."""
         return Vector2(self.x / other, self.y / other)
 
     def __rmul__(self, other):
+        """Multiplies a scalar by a vector."""
         return self.__mul__(other)
 
     def __abs__(self):
+        """Returns the magnitude (length) of the vector."""
         return (self*self) ** 0.5
 
     def __eq__(self, other):
+        """Checks if two vectors are equal."""
         return (self.x == other.x) and (self.y == other.y)
 
     def cross(self, other) -> float:
+        """Computes the cross product of two vectors."""
         return self.x*other.y - self.y*other.x
 
     def rotate(self, angle):
